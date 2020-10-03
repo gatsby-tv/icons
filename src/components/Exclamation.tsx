1b35a0085864e8d851c4acc1ed2ee7e26949662a
Calvin Jay Ross <calvinjayross@gmail.com>
2020-10-02 20:40:01 -0500

    added Exclamation

---
+++ src/components/Exclamation.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+function SvgExclamation() {
+  return (
+    <svg
+      id="gz-svg-error"
+      aria-hidden="true"
+      viewBox="0 0 540 540"
+      width="1em"
+      height="1em"
+    >
+      <g id="error" fill="currentcolor">
+        <path
+          id="exclamation-point"
+          d="M238.406 106.09v196.69h65.563V106.09h-65.563zm0 262.256v65.564h65.563v-65.564h-65.563z"
+        />
+        <path
+          id="border"
+          d="M270.016.031C121.263.031.029 121.257.029 270.006c0 148.748 121.234 269.976 269.987 269.976C418.768 539.982 540 418.754 540 270.006 540 121.257 418.768.03 270.016.03zm0 60C386.343 60.031 480 153.685 480 270.006c0 116.32-93.657 209.976-209.984 209.976-116.328 0-209.987-93.655-209.987-209.976S153.69 60.03 270.016 60.03z"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default SvgExclamation;
