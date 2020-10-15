7eba696db03e7cb380c17797cd0f6c6f265f9b2a
Calvin Jay Ross <calvinjayross@gmail.com>
2020-10-14 19:05:50 -0500

    added various ticks

---
+++ src/components/DownTick.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgDownTick() {
+  return (
+    <svg
+      id="gz-svg-down-tick"
+      aria-hidden="true"
+      viewBox="0 0 540 540"
+      width="1em"
+      height="1em"
+    >
+      <path
+        id="down-tick"
+        fill="currentcolor"
+        d="M0 135.012h540L270 404.988z"
+      />
+    </svg>
+  );
+}
+
+export default SvgDownTick;
---
+++ src/components/UpTick.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function SvgUpTick() {
+  return (
+    <svg
+      id="gz-svg-up-tick"
+      aria-hidden="true"
+      viewBox="0 0 540 540"
+      width="1em"
+      height="1em"
+    >
+      <path id="up-tick" fill="currentcolor" d="M540 404.988H0l270-269.976z" />
+    </svg>
+  );
+}
+
+export default SvgUpTick;
