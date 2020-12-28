b883060f2b54e6eeb7bee581a5430ae6ba7312f5
Calvin Jay Ross <calvinjayross@gmail.com>
2020-12-28 17:47:36 -0600

    added login, plug, and gear

---
+++ src/components/Plus.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgPlus() {
+  return (
+    <svg
+      id="gz-svg-plus"
+      aria-hidden="true"
+      viewBox="0 0 540 540"
+      width="1em"
+      height="1em"
+    >
+      <path
+        id="plus"
+        fill="currentcolor"
+        d="M230 0v230H0v80h230v230h80V310h230v-80H310V0h-80z"
+      />
+    </svg>
+  );
+}
+
+export default SvgPlus;
