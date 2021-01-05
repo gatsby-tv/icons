6d0372b39972578585abcc80299cc66cd4065013
Calvin Jay Ross <calvinjayross@gmail.com>
2021-01-05 02:52:07 -0600

    added next and previous

---
+++ src/components/Next.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgNext() {
+  return (
+    <svg
+      id="gz-svg-next"
+      aria-hidden="true"
+      viewBox="0 0 540 540"
+      width="1em"
+      height="1em"
+    >
+      <path
+        id="next"
+        fill="currentcolor"
+        d="M0 0v540l467.654-270L0 0zm467.654 270v270H540V0h-72.346v270z"
+      />
+    </svg>
+  );
+}
+
+export default SvgNext;
---
+++ src/components/Previous.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgPrevious() {
+  return (
+    <svg
+      id="gz-svg-previous"
+      aria-hidden="true"
+      viewBox="0 0 540 540"
+      width="1em"
+      height="1em"
+    >
+      <path
+        id="previous"
+        fill="currentcolor"
+        d="M540 0v540L72.346 270zM72.346 270v270H0V0h72.346z"
+      />
+    </svg>
+  );
+}
+
+export default SvgPrevious;
